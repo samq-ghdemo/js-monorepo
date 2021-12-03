5e60216616fd317d98ce7437a416865ac17ba3e0
Sam Quakenbush <sam.quakenbush@whitesourcesoftware.com>
2021-12-03 23:55:44 +0000

    remove dvna & security adventure

--- security-adventure/test/lib/index.js
+++
@@ -1,7 +0,0 @@
-var util = require('./util');
-
-exports.serve = util.serve;
-exports.level = util.level;
-exports.getApp = util.getApp;
-exports.wdtest = require('./wdtest');
-exports.appRequest = require('./app-request');
